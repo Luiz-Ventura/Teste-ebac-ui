21ea9882f6140045d58fed59104d294f7dfa55da
Luiz Ventura <luizcbnjr2001@gmail.com>
2025-06-05 15:53:44 -0300

    massa de dados

--- cypress/e2e/3-loja-ebac/login.cy.js
+++ cypress/e2e/3-loja-ebac/login.cy.js
@@ -1,9 +1,9 @@
 /// <reference types="cypress"/>
-
+const perfil = require('../../fixtures/perfil.json')
 describe('Funcionalidade Login', () =>{
 
 beforeEach(() => {
-    cy.visit('http://lojaebac.ebaconline.art.br/minha-conta/')
+    cy.visit('minha-conta')
 });
 afterEach(() => {
     cy.screenshot()
@@ -30,4 +30,18 @@ it('Deve exibir uma mensagem de erro ao inserir senha invalida ', () => {
     cy.get('.woocommerce-error'). should('contain' , 'Erro: A senha fornecida para o e-mail luiz@teste.com.br está incorreta.')
     cy.get('.woocommerce-error'). should('exist')
 });
+it('Deve fazer login com sucesso - usando massa de dados', () => {
+    cy.get('#username').type(perfil.usuario)
+    cy.get('#password').type(perfil.senha)
+    cy.get('.woocommerce-form > .button').click()
+    cy.get('.woocommerce-MyAccount-content > :nth-child(2)'). should('contain' , 'Olá, luiz (não é luiz? Sair)' )
+});
+it('Deve fazer login com sucesso - usando Fixture', () => {
+    cy.fixture('perfil').then( dados => {
+       cy.get('#username').type(dados.usuario, {log: false})
+       cy.get('#password').type(dados.senha, {log: false})
+       cy.get('.woocommerce-form > .button').click()
+       cy.get('.woocommerce-MyAccount-content > :nth-child(2)'). should('contain' , 'Olá, luiz (não é luiz? Sair)' )
+    })
+})
 })
--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -2,11 +2,12 @@
 
 describe('Funcionalidade: Produtos', () => {
     beforeEach(() => {
-        cy.visit('http://lojaebac.ebaconline.art.br/produtos/')
+        cy.visit('produtos')
     });
     it('deve selecionar um produto da lista', () => {
-        cy.get('.product-block ').eq(5).click()
-        cy.get('#tab-title-description > a').should('contain' , 'Descrição')
-        
+        cy.get('.block-inner')
+        .first()
+        .click()
+        cy.get('#tab-title-description > a'). should('contain' , 'Descrição')
     });
 });
