718fa71bb88b8d243a88c761f68c2e23c3f46a5a
Luiz Ventura <luizcbnjr2001@gmail.com>
2025-06-05 18:00:48 -0300

    page objects

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -1,13 +1,29 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/page-objects/produtos.page";
 
 describe('Funcionalidade: Produtos', () => {
     beforeEach(() => {
-        cy.visit('produtos')
+        produtosPage.visitarUrl()
     });
     it('deve selecionar um produto da lista', () => {
         cy.get('.block-inner')
         .first()
         .click()
         cy.get('#tab-title-description > a'). should('contain' , 'Descrição')
+    });
+    it('Deve selecionar o produto da lista procurando pelo nome', () => {
+        produtosPage.buscarProdutoLista('Abominable Hoodie')
+        cy.get('#tab-title-description > a'). should('contain' , 'Descrição')
+    });
+    it.only('deve buscar um produto com  sucesso usando o buscador', () => {
+        let produto ='Ajax Full-Zip Sweatshirt'
+        produtosPage.buscarProduto(produto)
+        cy.get('.product_title').should('contain' , produto)
+    });
+    it('deve vistar a pagina do produto', () => {
+        
+    });
+    it('deve adicionar o produto ao carrinho ', () => {
+        
     });
 });
