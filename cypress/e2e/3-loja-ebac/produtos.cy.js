635af3bfeeb3aeeb53b57be5b888dcca77a4e1f1
Luiz Ventura <luizcbnjr2001@gmail.com>
2025-06-06 17:35:30 -0300

    Automação e2e

--- cypress/e2e/3-loja-ebac/produtos.cy.js
+++ cypress/e2e/3-loja-ebac/produtos.cy.js
@@ -15,15 +15,30 @@ describe('Funcionalidade: Produtos', () => {
         produtosPage.buscarProdutoLista('Abominable Hoodie')
         cy.get('#tab-title-description > a'). should('contain' , 'Descrição')
     });
-    it.only('deve buscar um produto com  sucesso usando o buscador', () => {
+    it('deve buscar um produto com  sucesso usando o buscador', () => {
         let produto ='Ajax Full-Zip Sweatshirt'
         produtosPage.buscarProduto(produto)
         cy.get('.product_title').should('contain' , produto)
     });
     it('deve vistar a pagina do produto', () => {
-        
+        produtosPage.visitarProduto('Aero Daily Fitness Tee')
     });
     it('deve adicionar o produto ao carrinho ', () => {
+        let qtd = 10
+        produtosPage.buscarProduto('Geo Insulated Jogging Pant')
+        produtosPage.addProdutoCarrinho('34', 'Green', qtd)
+        cy.get('.woocommerce-message').should('contain', qtd + '× “Geo Insulated Jogging Pant” foram adicionados no seu carrinho.')
+    });
+    it.only('deve adicionar o produto ao carrinho buscando da massa de dados ', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarProduto(dados[5].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[5].tamanho, 
+            dados[5].cor, 
+            dados[5].quantidade)
+        cy.get('.woocommerce-message').should('contain', dados[5].nomeProduto)
         
     });
+
 });
+})
--- cypress/support/page-objects/produtos.page.js
+++ cypress/support/page-objects/produtos.page.js
@@ -11,11 +11,18 @@ class ProdutosPage{
         .contains(nomeProduto)
         .click()
     }
-    vistarProduto(){
-
+    visitarProduto(nomeProduto){
+    //cy.visit(`produtos/${nomeProduto}`)
+    const urlFormatada = nomeProduto.replace(/ /g, '-')
+    cy.visit(`produtos/${urlFormatada}`)
     }
-    addProdutoCarrinho(){
-
+    addProdutoCarrinho(tamanho, cor, quantidade){
+    cy.get('.button-variable-item-' + tamanho).click()
+    cy.get(`.button-variable-item-${cor}`).click()
+    cy.get('.input-text').clear().type(quantidade)
+    cy.get('.single_add_to_cart_button').click()
+    
+    
     }
 }
 
